63e47726f9ddd56c7ff42ad2491a0708cde3088e
alicoder186 <uzmukinaruto79@gmail.com>
2025-06-10 18:54:20 +0500

    Add files via upload

---
+++ leetday19.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortedArrayToBST(self, nums):
+        if len(nums) == 0:
+            return None
+
+        mid = len(nums) //2
+        root = TreeNode(nums[mid]) 
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
+        
