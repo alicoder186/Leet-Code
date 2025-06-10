846f3898c5b594169c3f2079adc915d38b8a873b
alicoder186 <uzmukinaruto79@gmail.com>
2025-06-10 18:50:12 +0500

    Add files via upload

---
+++ leetday13.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from typing import ListNode
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            
+            else:
+                current = current.next
+
+        
+        return head
+
+        
